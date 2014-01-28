331e64564d6d753e1e9f3dba378492134653b78d
Sergio Carrasco Herranz (sergio-portatil) <scarrascoh@gmail.com>
2014-01-28 22:51:56 +0100

    - Added the Dropbox Sync API - Added permissions to Dropbox API in the AndroidManifest.xml - Added some new styles and resources (layouts, icons...) - Creating Dropbox connection and files listing (70%)

--- src/com/sch/mydropboxelibrary/HomeActivity.java
+++ src/com/sch/mydropboxelibrary/HomeActivity.java
@@ -17,20 +17,29 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 package com.sch.mydropboxelibrary;
 
+import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxException.Unauthorized;
+import com.dropbox.sync.android.DbxFileSystem;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class HomeActivity extends Activity {
 
+	private DbxAccountManager mDbxAcctMgr;
+	final static int REQUEST_LINK_TO_DBX = 0;  // This value is up to you
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
-		//FIXME 0. Comprobar si ya se ha iniciado sesión
-		// En ese caso, ir al layout de la libreria
+		mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), 
+				"iserf0jchr78w3e", "qv6mo1h43oqgdlj");
 	}
 
 	@Override
@@ -45,7 +54,49 @@ public class HomeActivity extends Activity {
 	 * @param view
 	 */
 	public void onClickLoginBtn(View view){
-		//FIXME 0.0 Implementar
+		/* Check if the user is linked with Dropbox */
+		if(!mDbxAcctMgr.hasLinkedAccount()){
+			mDbxAcctMgr.startLink((Activity)this, REQUEST_LINK_TO_DBX);
+		}else{//Already authenticated
+			listEBooks();
+		}
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_LINK_TO_DBX) {
+	        if (resultCode == Activity.RESULT_OK) {// Start using Dropbox files.
+	        	listEBooks();
+	        } else {//Link failed or was cancelled by the user.
+	        	showErrorLoginMessage(getString(R.string.invalid_login));
+	        }
+	    } else {
+	        super.onActivityResult(requestCode, resultCode, data);
+	    }
+	}
+	
+	
+	/**
+	 * Change the layout and lList all e-books with extension .epub 
+	 * in the Dropbox current folder
+	 */
+	private void listEBooks(){
+		try {
+			DbxFileSystem dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+			//TODO 1. Seguir aqui 2
+		} catch (Unauthorized e) {
+			Log.e("exception", "Unathourized to access to files");
+			showErrorLoginMessage(getString(R.string.no_access));
+		}
+	}
+	
+	/**
+	 * Show an error message in the login layout
+	 * @param msg the error message to show
+	 */
+	private void showErrorLoginMessage(String msg){
+		
+	}
+	
+	
 }
