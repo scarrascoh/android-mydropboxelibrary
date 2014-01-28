05c277c694a7944033e7885d89afd1c14bf119a5
Sergio Carrasco Herranz (sergio-portatil) <scarrascoh@gmail.com>
2014-01-28 11:27:55 +0100

    Created Android Project
    Signed-off-by: Sergio Carrasco Herranz (sergio-portatil)
    <scarrascoh@gmail.com>

---
+++ src/com/sch/mydropboxelibrary/HomeActivity.java
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2014  Sergio Carrasco Herranz
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+package com.sch.mydropboxelibrary;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
